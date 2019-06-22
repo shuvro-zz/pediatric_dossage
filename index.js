5a637b018cf7fd4684ca2a0908a82b23e7702731
Alexis Salcedo <alexisss86@gmail.com>
2019-06-21 23:47:13 -0400

    inicio

---
+++ index.js
@@ -0,0 +1,31 @@
+document.querySelector("#calcular").addEventListener("click", function(){
+
+  //Variables para los calculos
+  const peso = document.getElementById("peso").value;
+  const dosis = document.getElementById("dosis").value;
+  const dosisDia = document.getElementById("dosisXDia").value;
+  const mL = document.getElementById("ml_del_medicamento").value;
+  const mG = document.getElementById("mg_del_medicamento").value;
+  const dosisMaxDiaria = peso * dosis;
+  const dosisMl = (dosisMaxDiaria * mL) / mG;
+  const dosisFinal = dosisMl / dosisDia;
+
+//Verificar si falta algun dato o resolver la dosis
+if (peso === "" || dosis === "" || dosisDia === "" || mL === "" || mG === ""){
+  document.querySelector("#alert").classList.add("unhide");
+  document.querySelector("#alert_message").textContent = "Parece que te falta algun dato, por favor verificalos";
+} else {
+  //Agregar clase para alerta con el resultado
+  document.querySelector("#alert").classList.add("unhide");
+
+  //Mensaje con el resultado
+  document.querySelector("#alert_message").textContent = "La dosis del medicamento es de " + dosisFinal.toFixed(2) + " ml cada " + 24/dosisDia + " horas.";
+
+//Resetear valores de los input luego de calcular
+  document.getElementById("dosis_form").reset();
+}})
+
+//Remover clase para alerta con el resultado
+document.querySelector("#alert_close").addEventListener("click", function(){
+  document.querySelector("#alert").classList.remove("unhide");
+})

988296791ed5b4f9c6e3826b47d2faf5f3a92892
Alexis Salcedo <alexisss86@gmail.com>
2019-07-05 22:20:10 -0400

    Workbox update

--- sw.js
+++ sw.js
@@ -26,22 +26,78 @@ workbox.routing.registerRoute(
 );
 
 
+if ('serviceWorker' in navigator) {
+  const wb = new Workbox('/sw.js');
+
+  // Add an event listener to detect when the registered
+  // service worker has installed but is waiting to activate.
+  wb.addEventListener('waiting', (event) => {
+    // `event.wasWaitingBeforeRegister` will be false if this is
+    // the first time the updated service worker is waiting.
+    // When `event.wasWaitingBeforeRegister` is true, a previously
+    // updated same service worker is still waiting.
+    // You may want to customize the UI prompt accordingly.
+
+    // Assumes your app has some sort of prompt UI element
+    // that a user can either accept or reject.
+    const prompt = createUIPrompt({
+      onAccept: async () => {
+        // Assuming the user accepted the update, set up a listener
+        // that will reload the page as soon as the previously waiting
+        // service worker has taken control.
+        wb.addEventListener('controlling', (event) => {
+          window.location.reload();
+        });
+
+        // Send a message telling the service worker to skip waiting.
+        // This will trigger the `controlling` event handler above.
+        // Note: for this to work, you have to add a message
+        // listener in your service worker. See below.
+        wb.messageSW({type: 'SKIP_WAITING'});
+      },
+
+      onReject: () => {
+        prompt.dismiss();
+      }
+    })
+  });
+
+  wb.register();
+}
+
+
 workbox.precaching.precacheAndRoute([
   {
+    "url": "anestesia.html",
+    "revision": "c7facac7e052d586119997d7355c7bfc"
+  },
+  {
+    "url": "antibioticos.html",
+    "revision": "3839adad906d518a6203bbb20132c8a1"
+  },
+  {
     "url": "css/style.css",
-    "revision": "8b104e76dc666a154dac2d46e4a2ce87"
+    "revision": "c0098949712d0f9f8f73692d5b9b4d72"
   },
   {
     "url": "index.html",
-    "revision": "2e9b849ead29e7c8f61551890b136f96"
+    "revision": "a44836ceb2b30e5deda4f4716531a087"
   },
   {
     "url": "js/index.js",
-    "revision": "d3ce87f7c7f306b80b4c8ae58da27889"
+    "revision": "2fdf177169b6a847c0dbc8bb7a9ebe15"
+  },
+  {
+    "url": "notas.html",
+    "revision": "39ec0738ae75cb4a7252a388f7dcff80"
+  },
+  {
+    "url": "preventiva.html",
+    "revision": "b7f4c0989667617c28d9f71f5aafa495"
   },
   {
     "url": "src-sw.js",
-    "revision": "8c105c33348448389064a6a05f4a13fe"
+    "revision": "eff7327e4c3898ef076cddb5be8a3888"
   },
   {
     "url": "workbox-config.js",
